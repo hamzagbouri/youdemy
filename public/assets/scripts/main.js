724610ed97cdd8bbb04ae67fe9ae54632f3704be
Hamza GBOURI <hamzagbouri2004@gmail.com>
2025-01-14 12:15:45 +0100

    front

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,18 @@
+console.log("ffffffffffffffff");
+function toggleAnswer(button) {
+    const answer = button.nextElementSibling;
+    const icon = button.querySelector("i");
+
+    if (answer.classList.contains("hidden")) {
+        answer.classList.remove("hidden");
+        answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+        answer.style.maxHeight = "0";
+        setTimeout(() => {
+            answer.classList.add("hidden");
+        }, 300);
+    }
+
+    icon.classList.toggle("ri-arrow-down-s-line");
+    icon.classList.toggle("ri-arrow-up-s-line");
+}
